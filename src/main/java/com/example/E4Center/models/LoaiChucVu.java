5e174e434dd25a8f7f27c1d8ae246cf9e3bf0473
chaocaonhan <ccnhan1288@gmail.com>
2024-09-25 01:47:22 +0700

    set up to create db auto

--- src/main/java/com/example/E4Center/models/LoaiChucVu.java
+++ src/main/java/com/example/E4Center/models/LoaiChucVu.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class LoaiChucVu {
+public class LoaiChucVu{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
