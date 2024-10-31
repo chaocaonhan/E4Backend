b42c587517433819ec5aa761584ccc9d27f81e2c
chaocaonhan <ccnhan1288@gmail.com>
2024-11-01 03:20:37 +0700

    fix deploy url

--- src/main/java/com/example/E4Center/config/OpenApiConfig.java
+++ src/main/java/com/example/E4Center/config/OpenApiConfig.java
@@ -4,12 +4,10 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI().info(new Info().title("E4Center API").version("V1.0"))
